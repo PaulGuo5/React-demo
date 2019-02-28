89824ab5be454be15974424ba07b382cf7c47c73
Glitch (starter-react-template) <none>
2019-02-28 00:48:04 +0000

    🎹🐘 Checkpoint ./src/index.js:2768340/5525 ./public/index.html:2768340/419 ./src/index.css:2768340/2360

--- src/index.js
+++ src/index.js
@@ -2,115 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
+const { useState, Fragment } = React;
+
+function ChatHook() {
+  const [value, setInputValue] = useState('');
+  const [messages, setMessages] = useState([]);
+  
+  const updateValue = ({ target }) => setInputValue(target.value); 
+  const updateMessages = () => {
+    if (!value) {
+      return;
+    }
+    const time = new Date().toLocaleTimeString();
+    const newMessage = 
+      <NewMessage key={time} message={value} timeStamp={time} />;
+    
+    setMessages(prevState => ([newMessage, ...prevState]));
+    setInputValue('');
+  }
+  
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
+    <Fragment>
+      <TextField handleOnChange={updateValue} value={value} />
+      <button className="btn--submit" onClick={updateMessages}>Send</button>
+      {messages}
+    </Fragment>
   );
 }
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
+const TextField = ({ value, handleOnChange }) => (
+  <textarea placeholder="Write your message" onChange={handleOnChange} value={value} />
+)
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+const NewMessage = ({ message, timeStamp }) => (
+  <div className="message__container">
+    <div>{message}</div>
+    <div>{timeStamp}</div>
+  </div>
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
+ReactDOM.render(
+  <ChatHook />,
+  document.getElementById('app')
+);
